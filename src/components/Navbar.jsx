ed4691206cd807e1750e5207a05d0a221bef05d8
esraelif <elifesratunca@gmail.com>
2024-06-02 08:26:51 +0200

    "Added Hero component, modified Navbar component, removed hero (2).svg file, and updated index.css"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { links } from '../data';
 const Navbar = () => {
     return (
         <nav className='bg-emerald-100'>
-            <div className='mx-auto max-w-7xl px-8 py-4 flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-8'>
+            <div className='align-element py-4 flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-8'>
                 <h2 className='text-3xl font-bold'>Esra<span className='text-emerald-600'>Elif</span></h2>
                 <div className='flex gap-x-3'>
                     {links.map((link) => {
@@ -15,7 +15,7 @@ const Navbar = () => {
 
                 </div>
             </div>
-            navbar
+
         </nav>
     );
 }
